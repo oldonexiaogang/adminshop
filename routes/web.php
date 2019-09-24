b9eec878429c6035875558244e89b1dd8e979bb2
xiaogang <646725232@qq.com>
2019-09-24 16:56:15 +0800

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::redirect('/home', '/')->name('home');
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
 });
