98123ffeeab553412012516094e7527ab4940d0c
vagrant <vagrant@homestead>
2019-10-14 09:27:07 +0000

    众筹数据库模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,11 +10,19 @@ class Product extends Model
     //
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price','content'
+        'rating', 'sold_count', 'review_count', 'price','content' , 'type',
     ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
+
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+
     // 与商品SKU关联
     public function skus()
     {
@@ -33,5 +41,10 @@ class Product extends Model
             return $this->attributes['image'];
         }
         return \Storage::disk('public')->url($this->attributes['image']);
+    }
+
+     public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 }
