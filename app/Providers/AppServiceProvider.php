ffe6b92ee22c387f33fc7e44a2e032fdd693e82e
vagrant <vagrant@homestead>
2019-10-14 09:18:45 +0000

    分类结束

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -53,5 +53,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+         \View::composer(['products.index', 'products.show'], \App\Http\ViewComposers\CategoryTreeComposer::class);
     }
 }
